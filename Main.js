ef0d12b93a7ee215d53b2989ce574855eaee9e7e
12pavani248 <pavani.ritika@gmail.com>
2021-10-09 17:33:20 +0530

    Solve the Ifand Else

---
+++ Main.js
@@ -0,0 +1,30 @@
+console.log('Hello World!');
+x=25;
+y=14
+a=-x+y;
+console.log(a/x);
+if (x==y)
+{
+  a=-x+y
+}
+
+//Jame 12 chocolate
+//Smith 49 Chocolate
+//Andy 23 Chocolate
+Jame=12;
+Smith=49;
+Andy=23;
+if (Jame-Andy)
+{
+  Jame=Jame-Andy+4;
+  Jame=Jame-Smith+2;
+}
+console.log(Jame);
+console.log(Andy);
+console.log(Smith);
+
+else(Simth-Andy-Jame)
+{
+  Simth=Simth-Andy+7;
+  Simth=Simth-Jame+10;
+}
