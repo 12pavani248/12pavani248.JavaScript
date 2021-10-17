59742d53ebcc82d759bc4b43ad5be8beb6807172
12pavani248 <pavani.ritika@gmail.com>
2021-10-17 09:31:50 +0530

    HomeWork

--- Main.js
+++ Main.js
@@ -1,33 +1,17 @@
-console.log('Hello World!');
-x=25;
-y=14
-a=-x+y;
-console.log(a/x);
-if (x==y)
+//1.
+
+
+compayList=["Hero Motor Crop","TVS","Yamana","Royal Enfield","Bajaj Auto"];
+for(let Counter=0;counter<=4;counter=counter+1);
 {
-  a=-x+y
+ console.log(companyList[counter]);
 }
 
-//Jame 12 chocolate
-//Smith 49 Chocolate
-//Andy 23 Chocolate
-Jame=12;
-Smith=49;
-Andy=23;
-if (Jame-Andy)
+//2.
+Dance=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+DanceHours=[1,2,1,2,1,2];
+for (let Y=0;Y<=5;Y=Y+1);
 {
-  Jame=Jame-Andy+4;
-  Jame=Jame-Smith+2;
+  console.log(Dance[Y]);
+  console.log(DanceHours[Y]);
 }
-console.log(Jame);
-console.log(Andy);
-console.log(Smith);
-
-else(Simth-Andy-Jame)
-{
-  Simth=Simth-Andy+7;
-  Simth=Simth-Jame+10;
-}
-  console.log(Simth);
-  console.log(Jame);
-  console.log(Andy);
